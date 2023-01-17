5a7681f69e71f715fb5ee6b92e37161a56d22b9f
Justin Verthein <Justinmackay88@hotmail.com>
2023-01-17 11:53:21 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce((acc, currentValue) => {
+    if(currentValue.assembled) {
+        return acc + currentValue.battterAmount;
+    } else {
+        return acc;
+    }
+}, 31);
+    
